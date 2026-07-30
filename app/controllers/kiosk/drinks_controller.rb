faf10c44f85aa26b50f3d89948a9ff58de9fbd82
bosseschum <accounts@bschum.de>
2026-07-30 20:37:45 +0200

    fix reduced amounts logic for more than one crate

--- app/controllers/kiosk/drinks_controller.rb
+++ app/controllers/kiosk/drinks_controller.rb
@@ -22,12 +22,10 @@ class Kiosk::DrinksController < ApplicationController
         { product: current_organization.products.find(product_id), quantity: quantity }
       end
       @total_quantity = @cart_items.sum { |i| i[:quantity] }
+      @full_crates = @total_quantity / CRATE_SIZE
+      @single_products = @total_quantity % CRATE_SIZE
       @is_mixed_crate = @total_quantity == CRATE_SIZE
-      @cart_total = if @is_mixed_crate
-        CRATE_PRICE_CENTS
-      else
-        @cart_items.sum { |i| i[:product].price_cents * i[:quantity] }
-      end
+      @cart_total = calculate_total(@cart, current_organization)
     end
   end
 
@@ -65,13 +63,10 @@ class Kiosk::DrinksController < ApplicationController
     end
 
     total_quantity = cart.values.sum
-    is_mixed_crate = total_quantity == CRATE_SIZE
+    full_crate = total_quantity / CRATE_SIZE
+    single_product = total_quantity % CRATE_SIZE
 
-    einzelpreis_gesamt = cart.sum do |pid, qty|
-      current_organization.products.find(pid).price_cents * qty
-    end
-
-    total = is_mixed_crate ? CRATE_PRICE_CENTS : einzelpreis_gesamt
+    total = calculate_total(cart, current_organization)
 
     # Saldo-Check gilt nur für Members, nicht für Gäste (Gäste bekommen Rechnung)
     if @purchaser.is_a?(Member) && !@purchaser.can_purchase?(sponsored ? 0 : total)
@@ -81,6 +76,8 @@ class Kiosk::DrinksController < ApplicationController
 
     cart.each do |product_id, quantity|
       product = current_organization.products.find(product_id)
+      full_crate_for_product = quantity / CRATE_SIZE
+      single_product_for_product = quantity % CRATE_SIZE
       full_amount = product.price_cents * quantity
 
       if is_mixed_crate && !sponsored
@@ -90,17 +87,33 @@ class Kiosk::DrinksController < ApplicationController
         actual_amount = full_amount
       end
 
-      Transaction.create!(
-        purchaser:             @purchaser,
-        organization:          current_organization,
-        product:               product,
-        amount_cents:          sponsored ? 0 : -actual_amount,
-        original_amount_cents: full_amount,
-        kind:                  :drink_purchase,
-        quantity:              quantity,
-        sponsored:             sponsored,
-        note: "#{quantity}x #{product.name}#{is_mixed_crate ? " (Mischkasten)" : ""}#{sponsored ? " (gesponsert)" : ""}"
-      )
+      if full_crate_for_product > 0
+        part = full_crate_for_product * CRATE_PRICE_CENTS
+        Transaction.create!(
+          purchaser:             @purchaser,
+          product:               product,
+          amount_cents:          sponsored ? 0 : -part,
+          original_amount_cents: product.price_cents * quantity,
+          kind:                  :drink_purchase,
+          quantity:              full_crate_for_product * CRATE_SIZE,
+          sponsored:             sponsored,
+          note: "#{full_crate_for_product * CRATE_SIZE}x #{product.name} (#{full_crate_for_product} Kasten)"
+        )
+      end
+
+      if single_product_for_product > 0
+        part = single_product_for_product * product.price_cents
+        Transaction.create!(
+          purchaser:             @purchaser,
+          product:               product,
+          amount_cents:          sponsored ? 0 : -part,
+          original_amount_cents: part,
+          kind:                  :drink_purchase,
+          quantity:              single_product_for_product,
+          sponsored:             sponsored,
+          note: "#{single_product_for_product}x #{product.name}"
+        )
+      end
     end
 
     session[:cart] = {}
@@ -111,5 +124,16 @@ class Kiosk::DrinksController < ApplicationController
   def clear_cart
     session[:cart] = {}
     redirect_to kiosk_root_path(pin: params[:pin])
+  end
+
+  private
+
+  def calculate_total(cart, organization)
+    cart.sum do |product_id, quantity|
+      product = organization.products.find(product_id)
+      full_crate = quantity / CRATE_SIZE
+      single_product = quantity % CRATE_SIZE
+      (full_crate * CRATE_PRICE_CENTS) + (single_product * product.price_cents)
+    end
   end
 end
